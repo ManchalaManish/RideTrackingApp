e3ef2d27f9e13990e24b9de617f9a03f0085afdf
Subham Surmara <subhamsurmara@gmail.com>
2024-11-01 12:23:50 +0530

    initial commit

---
+++ src/main/java/com/example/RideTrackingApp/RideTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.RideTrackingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideTrackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RideTrackingApp/RideTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RideTrackingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
